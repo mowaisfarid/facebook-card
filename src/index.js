6b727883f819285e1401667bdc65b31f71188d0e
Muhammad zubair <mowaisfarid@gmail.com>
2021-01-29 18:13:41 +0500

    some work

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function FacebookAss({ smallImg, name, timeDate, post, postImage }) {
+  return (
+    <div>
+      <div className="smallimgDiv">
+        <img className="smallImg" src={smallImg} />
+        <h1 className="userName"> {name}</h1>
+        {/* <p className="timeDate">{timeDate}</p> */}
+      </div>
+
+      <p className="post">{post}</p>
+      <img className="postImage" src={postImage} />
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <FacebookAss
+      smallImg="https://lh3.googleusercontent.com/ogw/ADGmqu8L15yPCW-fIvRuMpN0fE9pKls2yY8xNelSM5XT9w=s83-c-mo"
+      name="Muddassir"
+      timeDate="Monday at 11:00 AM"
+      post="Don't stress me, You can't impress me"
+      postImage="https://www.setaswall.com/wp-content/uploads/2017/06/Dont-stress-me-you-cant-impress-me-851-x-315-768x284.jpg"
+    />
+  </div>,
+  document.querySelector("#root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
